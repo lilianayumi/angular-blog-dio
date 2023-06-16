1e28e9bd00e76098b0c16b817e1f0a9aa92b595c
lilianayumi <lilianayumi97@gmail.com>
2023-06-16 13:21:40 -0300

    Auto-generated commit [ci skip]

---
+++ assets/data/dataFake.ts
@@ -0,0 +1,26 @@
+export const dataFake = [
+  {
+    "id": "1",
+    "photo": "https://upload.wikimedia.org/wikipedia/commons/thumb/c/cf/Angular_full_color_logo.svg/250px-Angular_full_color_logo.svg.png",
+    "title": "Angular",
+    "description": "Oferece uma funcionalidade poderosa chamada two-way data binding, que permite que as alterações nos dados sejam refletidas automaticamente na interface do usuário e vice-versa, eliminando a necessidade de manipular manualmente a atualização da interface do usuário quando os dados são modificados, tornando o desenvolvimento mais eficiente e produtivo. Possui um sistema de injeção de dependência embutido, que facilita a gestão de dependências e promove a modularidade e testabilidade do código. Mantido pela equipe do Google, possui um ecossistema maduro e amplo, além de uma comunidade ativa que contribui com recursos adicionais e solução de problemas. A ferramenta Angular CLI, simplifica o processo de criação, desenvolvimento e teste de aplicativos, fornecendo uma estrutura padrão para a organização do projeto e geração de código automática."
+  },
+  {
+    "id": "2",
+    "photo": "https://logospng.org/download/react/logo-react-256.png",
+    "title": "React",
+    "description": "Utiliza um algoritmo chamado Virtual DOM para atualizar apenas as partes necessárias da interface do usuário, em vez de toda a árvore do DOM, melhorando o desempenho e a eficiência da aplicação. Possui uma comunidade ativa e um ecossistema vasto de bibliotecas, ferramentas e componentes de terceiros, oferecendo muitos recursos e soluções para acelerar o desenvolvimento de aplicações. É altamente flexível e pode ser utilizado tanto para a criação de componentes isolados em páginas web quanto para a construção de aplicações complexas em larga escala."
+  },
+  {
+    "id": "3",
+    "photo": "https://br.vuejs.org/images/logo.png",
+    "title": "Vue.js",
+    "description": "Conhecido por sua fácil aprendizagem e uso. Ele possui uma sintaxe intuitiva e uma documentação clara, o que facilita a compreensão e a adoção por parte dos desenvolvedores. O sistema de reatividade atualiza automaticamente a interface do usuário sempre que os dados são alteradossimplificando a manipulação e melhorando a eficiência."
+  },
+  {
+    "id": "4",
+    "photo": "https://aurelia.io/styles/images/aurelia-icon.svg",
+    "title": " Aurelia",
+    "description": "Possui uma abordagem 'convenção sobre configuração', o que significa que muitas decisões são tomadas por padrão, reduzindo a necessidade de configuração manual. É construído com uma arquitetura extensível, permitindo a adição ou substituição de recursos, proporcionando maior liberdade para personalizar a estrutura da aplicação de acordo com as necessidades do projeto. Projetado para funcionar perfeitamente com o padrão ES6 e versões posteriores, oferece suporte nativo a recursos modernos do JavaScript, como classes, módulos e promessas."
+  }
+]
